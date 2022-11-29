080731ab1033a6a615e582ef2a092816ec67d449
khan2876 <akhan@bpsk12.org>
2022-11-28 21:58:32 -0500

    initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] myArray = {4, 5, 6, 7};
+
+        System.out.println( arrayAverage(myArray) )
+        
+    }
+
+    public static double arrayAverage(int[] array) {
+        double sum = 0;
+
+        // add up the terms of the array
+        for (int i = 0; i < 5; i++) {
+
+        }
+
+        return sum / array.length;
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# About Me
+Name: 
+Class: 
+
+
+# Instructions
+1.  Pull this repo into VS Code.
+2.  Complete the About Me section in this README file and commit the change.
+3.  Fix the 
+4.  
